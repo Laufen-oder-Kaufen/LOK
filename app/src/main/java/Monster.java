30c483e5d638e96ed502f6b96b722834dc7ec3a5
SimonDolll <72542886+SimonDolll@users.noreply.github.com>
2021-01-22 10:45:17 +0100

    Create Monster

---
+++ app/src/main/java/Monster.java
@@ -0,0 +1,14 @@
+/***
+ * the monster class is a special actor that can hold exp
+ */
+public class Monster extends Actor{
+    private int exp;
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+}
