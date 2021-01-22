0e6354efdd6882cf121e40c0d069699aa8f80604
SimonDolll <72542886+SimonDolll@users.noreply.github.com>
2021-01-22 10:22:02 +0100

    Update Equip
    added constructors and getter to Equip

--- app/src/main/java/Equip.java
+++ app/src/main/java/Equip.java
@@ -5,4 +5,16 @@
  */
 public class Equip extends Actor{
     private int slot;
+
+    public Equip(int id, String name, String image, String icon, String type, int maxHP, int currHP, int str, int def, int mag, int mdf, int agi, int luk, int slot) {
+        super(id, name, image, icon, type, maxHP, currHP, str, def, mag, mdf, agi, luk);
+        this.slot = slot;
+    }
+
+    public Equip() {
+    }
+
+    public int getSlot() {
+        return slot;
+    }
 }
