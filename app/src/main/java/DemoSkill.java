8d07fca41daec6984d6e57df1da928ef26636b8e
Berkan Yücel <berkan13@schule.bremen.de>
2021-05-22 00:40:52 +0200

    merge conflict resolved

--- app/src/main/java/DemoSkill.java
+++
@@ -1,9 +0,0 @@
-/***
- * class to present what a skill might look like
- */
-public class DemoSkill extends Skill {
-    @Override
-    void skillEffect() {
-        System.out.println("DemoSkill wurde benutzt!");
-    }
-}
--- app/src/main/java/com/example/lok/Skill.java
+++ app/src/main/java/com/example/lok/Skill.java
@@ -8,22 +8,15 @@ package com.example.lok;
  * icon
  * cd: cool down time of the skill
  */
-<<<<<<< HEAD:app/src/main/java/Skill.java
-abstract class Skill extends MenuActor {
-=======
-public abstract class Skill extends MenuActor{
->>>>>>> ui:app/src/main/java/com/example/lok/Skill.java
+public abstract class Skill extends MenuActor {
+
     private int cd;
 
     abstract void skillEffect();
 
-<<<<<<< HEAD:app/src/main/java/Skill.java
-    public Skill() {
-=======
     public Skill(int id, String name, int image, int icon, int cd) {
         super(id, name, image, icon);
         this.cd = cd;
->>>>>>> ui:app/src/main/java/com/example/lok/Skill.java
     }
 
     public int getCd() {
