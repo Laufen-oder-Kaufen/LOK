2d7026d286468d6a212e2ba5653b7cf71bf645d7
deanomus <dean@reinhardt.work>
2021-05-21 11:42:59 +0200

    implement login function with restapi

--- app/src/main/java/com/example/lok/LoginActivity.kt
+++ app/src/main/java/com/example/lok/LoginActivity.kt
@@ -7,6 +7,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import de.deanomus.restapi.RestAPIData
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 class LoginActivity : AppCompatActivity() {
 
@@ -19,6 +26,36 @@ class LoginActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             // LoginFunktion
+
+
+            var reqParam = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(username.toString(), "UTF-8")
+            reqParam += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password.toString(), "UTF-8")
+            val mURL = URL(RestAPIData.url + "api/v1_0/login")
+
+            with(mURL.openConnection() as HttpURLConnection) {
+                // optional default is GET
+                requestMethod = "POST"
+
+                val wr = OutputStreamWriter(getOutputStream());
+                wr.write(reqParam);
+                wr.flush();
+
+                println("URL : $url")
+                println("Response Code : $responseCode")
+
+                BufferedReader(InputStreamReader(inputStream)).use {
+                    val response = StringBuffer()
+
+                    var inputLine = it.readLine()
+                    while (inputLine != null) {
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    println("Response : $response")
+                }
+            }
+
+
             Intent(this, MainActivity::class.java).also {
                 startActivity(it, null)
             }
